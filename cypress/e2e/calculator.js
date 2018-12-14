69c8f8fe7b02b68e201e46e167d0d18e22e9bed4
nikhilatjumpcut <nikhil@jumpcut.com>
2018-12-15 04:07:17 +0530

    Write test for authenticated user

--- cypress/e2e/calculator.js
+++ cypress/e2e/calculator.js
@@ -15,3 +15,24 @@ describe('anonymous calculator', () => {
       .should('have.text', '9')
   })
 })
+
+describe('authenticated calculator', () => {
+  it('displays the username', () => {
+    cy.createUser().then(user => {
+      cy.visit('/')
+        .getByText(/login/i)
+        .click()
+        .getByLabelText(/username/i)
+        .type(user.username)
+        .getByLabelText(/password/i)
+        .type(user.password)
+        .getByText(/submit/i)
+        .click()
+        .assertLoggedInAs(user)
+        .getByText(/logout/i)
+        .click()
+        .queryByTestId('username-display', {timeout: 300})
+        .should('not.exist')
+    })
+  })
+})
